cf8c75b3847186055bb09cd5b4e8d40b4e8afe22
Alisson Rech Honorato <92186539+alissonRH@users.noreply.github.com>
2021-10-24 13:08:08 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+const LinksSocialMedia = {
+  github: 'alissonRH',
+  instagram: 'alissonhonoratoo',
+  twitter: 'alissonRech',
+  facebook: 'alissonRech',
+  linkedin: 'alisson-rech-honorato-7906b5158/'
+}
+
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${LinksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      userImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfos()
